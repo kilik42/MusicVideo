49fbaf0b50f6e02ca6d6251d7de59bb207079a01
marvin evins <maevins@cps.edu>
2016-06-20 22:39:47 -0500

    added apimanager class

---
+++ MusicVideoApp/APIManager.swift
@@ -0,0 +1,24 @@
+//
+//  APIManager.swift
+//  MusicVideoApp
+//
+//  Created by marvin evins on 6/20/16.
+//  Copyright © 2016 marvin evins. All rights reserved.
+//
+
+import Foundation
+
+class APIManager {
+    
+    func loadData(urlString: String, completion: (result; String) -> Void){
+         
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
