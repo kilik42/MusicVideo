71414b91a614f80638b2af6ae0f65198443331b9
marvin evins <maevins@cps.edu>
2016-06-25 21:37:58 -0500

    created custom initializer

---
+++ MusicVideoApp/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MusicVideoApp
+//
+//  Created by marvin evins on 6/24/16.
+//  Copyright © 2016 marvin evins. All rights reserved.
+//
+
+import Foundation
+
+
+typealias JSONDictionary = [String: AnyObject]
+
+typealias JSONArray = Array<AnyObject>
+
