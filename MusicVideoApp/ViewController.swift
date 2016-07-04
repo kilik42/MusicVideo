bc1364a0f750f7cc635e47bfa0487bcd9f84ea86
marvin evins <maevins@cps.edu>
2016-07-04 08:03:38 -0500

    obj-c reachability view controller config

--- MusicVideoApp/ViewController.swift
+++ MusicVideoApp/ViewController.swift
@@ -13,11 +13,15 @@ class ViewController: UIViewController {
     
     var videos = [Videos]()
     
+    @IBOutlet var displayLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "reachabilityStatusChanged", name: "ReachStatusChanged", object: nil)
         
-        
+        reachabilityStatusChanged()
         
         let api = APIManager()
         api.loadData("https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json",
@@ -47,7 +51,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func reachabilityStatusChanged()
+    {
+        switch reachabilityStatus{
+            
+        case NOACCESS: view.backgroundColor = UIColor.redColor()
+            displayLabel.text = "No Internet"
+        case WIFI : view.backgroundColor = UIColor.greenColor()
+            displayLabel.text = "Reachable with WIFI"
+        case WWAN : view.backgroundColor = UIColor.yellowColor()
+            displayLabel.text = "Reachable with Cellular"
+            
+        default: return
+        }
+    }
     
+    // is called just as the object is about to be deallocated
+    deinit
+    {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
+    }
    
 
 }
